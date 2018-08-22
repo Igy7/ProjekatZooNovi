5358e1c55500c4a6f02d2568c241959fafc34bfe
Igy7 <Igy7@users.noreply.github.com>
2018-08-22 03:49:18 +0200

    KorisnikCeo
    jedva

---
+++ ZooVrt/Areas/Korisnik/Models/SmallButtonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace ZooVrt.Areas.Korisnik.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        public int? rezervacijaId { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                var param = new StringBuilder("?");
+
+                if (rezervacijaId != null && rezervacijaId > 0)
+                    param.Append(String.Format("{0}={1}&", "id", rezervacijaId));
+
+                return param.ToString().Substring(0, param.Length - 1);
+            }
+        }
+
+
+    }
+}
